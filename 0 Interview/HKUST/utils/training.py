8424940257d908b6d4c6514eeda64139f4f3b147
tekboart <tekboart@gmail.com>
2023-07-06 16:18:56 +0330

    (1) added loss to the metrics, (2) now my main train_model returns all metrics as a dict (just like TF's history.history), (3) minor change to training printing log/info

--- 0 Interview/HKUST/utils/training.py
+++ 0 Interview/HKUST/utils/training.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+from typing import Union
 
 import os
 from tqdm import tqdm
@@ -66,7 +67,8 @@ def train_fn(
     # tqdm() returns an iterator so never access its content to avoid exhaustion
     # that's why we wrapped it in iter()
     # count the #iteration/steps in each epoch
-    step_counter = sum(1 for _ in iter(loop))
+    num_batches = sum(1 for _ in iter(loop))
+    epoch_loss_cum = 0
 
     for batch_idx, (data, targets) in enumerate(loop):
         data = data.to(device)
@@ -85,7 +87,7 @@ def train_fn(
         # calc eval metrics (for training)
         for key in metrics:
             if eval_fn := metrics_fn.get(key):
-                metrics[key] += eval_fn(predictions, targets)
+                metrics[key] += eval_fn(predictions, targets).item()
 
         # backprop
         # init all grads az zero/0
@@ -95,15 +97,23 @@ def train_fn(
         scaler.step(optimizer)
         scaler.update()
 
+        # save the loss (for this iteration/step/mini_batch)
+        batch_loss = loss.item()
+        epoch_loss_cum += batch_loss
+
         # update the tqdm loop
-        loop.set_postfix(loss=loss.item())
+        loop.set_postfix(loss=batch_loss)
+
+    # add the loss (of this epoch) to metrics
+    metrics['loss'] = epoch_loss_cum
 
     for key in metrics:
-        metrics[key] /= step_counter
+        # divide all metrics by the #steps/batches
+        metrics[key] /= num_batches
 
     return metrics
 
-
+# TODO: This should work standalone (for evaluation of test data)
 def validation_fn(
     loader,
     model,
@@ -128,6 +138,7 @@ def validation_fn(
     # probab to set training=False, so do only forward
     model.eval()
 
+    # TODO: do validation_fn in mini_batch style for mor vectorization (it seems to predict one example at a time)
     # Don't cache values for backprop (ME)
     with torch.no_grad():
         for x, y in loader:
@@ -179,7 +190,7 @@ def train_model(
     save_checkpoint_name: str = None,
     load_model: bool = False,
     load_checkpoint_path: str = None,
-    metrics: tuple = (),
+    metrics: Union[tuple, list] = (),
     metrics_fn: dict = {},
 ):
     """
@@ -198,34 +209,54 @@ def train_model(
     # when precision lvl (e.g., Float16) cannot represent very small numbers
     scaler = torch.cuda.amp.GradScaler()
 
+    # init the train metrcis (e.g., val_loss, val_dice, etc.)
+    # we add 'loss' separately as it's not part of metrics
+    history = {'loss': []}
+    # add other metrics (if any)
+    for key in metrics:
+        history[key] = []
+
+    # init the val metrcis (e.g., val_loss, val_dice, etc.)
+    if val_loader:
+        val_keys = []
+        for key in history:
+            val_keys.append(f'val_{key}')
+        for key in val_keys:
+            history[key] = []
+
     for epoch in range(epochs):
-        # create dict from metrics list/tuple
+        print(f" epoch {epoch+1}/{epochs} ".center(79, "-"))
+        # create/reset metrics values to 0 (for each epoch)
         metrics = dict.fromkeys(metrics, 0)
 
         # Start training iterations (for this epoch)
-        print(" Training Phase (In Progress) ".center(79, "-"))
+        # print(" Training Phase (In Progress) ".center(79, "-"))
         train_metrics = train_fn(
             train_loader, model, optimizer, loss_fn, scaler, metrics, metrics_fn, device
         )
 
         # plot the validation metrics
-        print(f" epoch {epoch}'s metric(s) (training) ".center(79, "."))
+        # print(f" epoch {epoch}'s metric(s) (training) ".center(79, "."))
+        print()
         for key, value in train_metrics.items():
-            print(f"{key:<10} {value.item():>5.2f}")
+            print(f"{key+':':<15} {value:>5.2f}")
+            history[key].append(value)
         # print(" Training Phase (Done) ".center(79, "-"))
 
         if val_loader:
             # TODO: use metrics dict (but with 'val_' prefix) to automate things
-            print(" Validation Phase (In Progress) ".center(79, "-"))
+            # print(" Validation Phase (In Progress) ".center(79, "-"))
 
             val_accu, val_dice = validation_fn(
                 val_loader, model, metrics_fn=metrics_fn, device=device
             )
 
             # plot the validation metrics
-            print(f" epoch {epoch}'s metric(s) (validation) ".center(79, "."))
-            print(f'{"val_accuracy:":<10} {val_accu.item():>5.2f}')
-            print(f'{"val_dice:":<10} {val_dice.item():>5.2f}')
+            # print(f" epoch {epoch}'s metric(s) (validation) ".center(79, "."))
+            print(f'{"val_accuracy:":<15} {val_accu.item():>5.2f}')
+            print(f'{"val_dice:":<15} {val_dice.item():>5.2f}')
+            history['val_accuracy'].append(val_accu.item())
+            history['val_dice'].append(val_dice.item())
             # print(" Validation Phase (Done) ".center(79, "-"))
 
             # print some examples to a folder
@@ -240,6 +271,8 @@ def train_model(
         save_checkpoint(checkpoint, dirname=save_checkpoint_path, filename=save_checkpoint_name)
 
     #
+
+    return history
 
 
 # TODO: write a main fn for pytorch-lightning
