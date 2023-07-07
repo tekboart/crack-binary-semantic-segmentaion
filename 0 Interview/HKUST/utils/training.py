47f85001a2c7dbf27c65e3f91e82f9445aa88f63
tekboart <tekboart@gmail.com>
2023-07-08 00:02:34 +0330

    (1) added lr_scheduler (LRStep, every 5 epoch, gammar=0.5 ), (2) fixed high val_loss (culprit: had made yhat binary and used with BCEWithLogitLoss), (3) changed tqm a bit, (4) minor refactoring

--- 0 Interview/HKUST/utils/training.py
+++ 0 Interview/HKUST/utils/training.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
 
 from typing import Union
 
@@ -57,44 +58,48 @@ def train_fn(
     loss_fn,
     scaler,
     from_logits: bool,
-    metrics: dict = {},
-    metrics_fn: dict = {},
+    metrics: list,
+    metrics_fn: dict,
+    epoch: int,
     device: str = "cuda:0",
 ):
     """
     does one epoch of training
+        includes (1) forwards pass, (2) calc loss, (3) metrics (if any), (4) backprop
     """
     loop = tqdm(loader)
+    loop.set_description(f"Epoch {epoch+1}")
     # tqdm() returns an iterator so never access its content to avoid exhaustion
     # that's why we wrapped it in iter()
     # count the #iteration/steps in each epoch
     num_batches = sum(1 for _ in iter(loop))
     epoch_loss_cum = 0
 
-    for batch_idx, (data, targets) in enumerate(loop):
-        data = data.to(device)
-        targets = targets.float().to(device)
+    for batch_idx, (x, y) in enumerate(loop):
+        # load our x:data, y:targets to device's MEM (e.g., GPU VRAM)
+        x = x.float().to(device)
+        y = y.float().to(device)
 
         # forward
         # we use float16 to reduce VRAM and MEM usage
         with torch.cuda.amp.autocast():
-            predictions = model(data)
-            # must check the shape of preds and target masks before giving them to loss_fn
-            assert predictions.shape == targets.shape
+            yhat = model(x)
+            assert yhat.shape == y.shape
 
             # calc the loss
-            loss = loss_fn(predictions, targets)
+            loss = loss_fn(yhat, y)
 
-        # calc eval metrics (for training)
+        # calc train metrics for this mini_batch
         for key in metrics:
             if eval_fn := metrics_fn.get(key):
-                metrics[key] += eval_fn(predictions, targets, from_logits).item()
+                metrics[key] += eval_fn(yhat, y, from_logits).item()
 
         # backprop
         # init all grads az zero/0
         optimizer.zero_grad()
-        # prevent underflow of very small loss values
+        # calc grads + use scaler to prevent underflow
         scaler.scale(loss).backward()
+        # update weights + use scaler to prevent underflow
         scaler.step(optimizer)
         scaler.update()
 
@@ -119,73 +124,106 @@ def train_fn(
 def validation_fn(
     loader,
     model,
+    loss_fn,
     from_logits: bool,
-    num_classes: int = 1,
-    thresh: float = 0.5,
-    metrics: dict = {},
-    metrics_fn: dict = {},
+    metrics: dict,
+    metrics_fn: dict,
     device: str = "cuda",
 ):
     """
     does one validation step (used at the end of each epoch)
         simply, does (1) forward pass + (2) eval_metrics (for the val set)
     """
-
-    # TODO: make it modular by taking the metrics and metrics_fn args
-    num_correct = 0
-    num_pixels = 0
-    dice_score = 0
+    num_val_batches = sum(1 for _, _ in loader)
+    epoch_val_loss_cum = 0
 
     # TODO: why this line?
     # probab to set training=False, so do only forward
     model.eval()
 
-    # TODO: do validation_fn in mini_batch style for mor vectorization (it seems to predict one example at a time)
+    # TODO: do validation_fn in mini_batch style for more vectorization (it seems to predict one example at a time)
     # Don't cache values for backprop (ME)
     with torch.no_grad():
         for x, y in loader:
             # load our x:data, y:targets to device's MEM (e.g., GPU VRAM)
-            x = x.to(device)
-            y = y.to(device)
+            x = x.float().to(device)
+            y = y.float().to(device)
 
-            # calc predictions
-            # if used act_fn on the last layer no need for this line
-            if from_logits:
-                preds = torch.sigmoid(model(x))
-            else:
-                preds = model(x)
+            # forward
+            # we use float16 to reduce VRAM and MEM usage
+            with torch.cuda.amp.autocast():
+                yhat = model(x)
+                assert yhat.shape == y.shape
 
-            # make pixel values for predictions binary
-            # a pixel is either part of a class or not
-            # use .float() to have 0.0/1.0 as our data are of type float not int
-            preds = (preds > thresh).float()
-            # calc #pixels in this img_batch that were classified correctly
-            num_correct += (preds == y).sum()
-            # calc the total #pixels in this img_batch
-            num_pixels += torch.numel(preds)
+                # calc the loss
+                loss = loss_fn(yhat, y)
 
-            # calc dice score
-            # method 1: (didn't work)
-            # dice_score += (2 * (preds * y).sum()) / (preds + y).sum() + 1e-8
-            # method 2
-            dice_score += metrics_fn["dice"](preds, y, from_logits)
+                # save the loss (for this iteration/step/mini_batch)
+                batch_val_loss = loss.item()
+                epoch_val_loss_cum += batch_val_loss
 
-    accu = num_correct / num_pixels
-    dice = dice_score / len(loader)
+            # calc val metrics for this mini_batch
+            for key in metrics:
+                # used split() to remove 'val_' part of key
+                if eval_fn := metrics_fn.get(key.split("_")[1]):
+                    metrics[key] += eval_fn(yhat, y, from_logits).item()
+
+    # add the val_loss (of this epoch) to metrics
+    metrics["val_loss"] = epoch_val_loss_cum
+
+    for key in metrics:
+        # divide all metrics by the #steps/batches
+        metrics[key] /= num_val_batches
 
     # TODO: why this line?
     # probab to set training=True (for future training)
     model.train()
 
-    return accu, dice
+    return metrics
+
+
+# TODO: create a class with fit(), evaluate(), predict() methods
+class BinarySegmentationModel(nn.Module):
+    def __init__(
+        self,
+        model,
+        train_loader,
+        from_logits: bool,
+        epochs: int = 5,
+        val_loader=None,
+        lr_rate: float = 0.001,
+        device: str = "cuda:0",
+        save_model: bool = False,
+        save_checkpoint_path: str = None,
+        save_checkpoint_name: str = None,
+        load_model: bool = False,
+        load_checkpoint_path: str = None,
+        metrics: Union[tuple[str], list[str]] = (),
+        metrics_fn: dict = {},
+    ):
+        super().__init__()
+        self.model = model
+        self.train_loader = train_loader
+        self.from_logits = from_logits
+        self.epochs = epochs
+        self.val_loader = val_loader
+        self.lr = lr_rate
+        self.device = device
+        self.save_model = save_model
+        self.save_checkpoint_path = save_checkpoint_path
+        self.save_checkpoint_name = save_checkpoint_name
+        self.load_model = load_model
+        self.load_checkpoint_path = load_checkpoint_path
+        self.metrics = metrics
+        self.metrics_fn = metrics_fn  # TODO: can make it universal (no need to input it to the class.__init__)
 
 
 def train_model(
     model,
     train_loader,
-    val_loader,
-    epochs: int,
     from_logits: bool,
+    epochs: int = 5,
+    val_loader=None,
     lr: float = 0.001,
     device: str = "cuda:0",
     save_model: bool = False,
@@ -193,7 +231,7 @@ def train_model(
     save_checkpoint_name: str = None,
     load_model: bool = False,
     load_checkpoint_path: str = None,
-    metrics: Union[tuple, list] = (),
+    metrics: Union[tuple[str], list[str]] = (),
     metrics_fn: dict = {},
 ):
     """
@@ -210,6 +248,8 @@ def train_model(
 
     # set the optimizer
     optimizer = optim.Adam(model.parameters(), lr=lr)
+    # set learning schedualer
+    scheduler = StepLR(optimizer, step_size=5, gamma=0.5)
 
     # load weights a pretrained model
     if load_model:
@@ -234,14 +274,11 @@ def train_model(
         for key in val_keys:
             history[key] = []
 
+    # loop over #epochs
     for epoch in range(epochs):
         print(f" epoch {epoch+1}/{epochs} ".center(79, "-"))
         # create/reset metrics values to 0 (for each epoch)
-        metrics = dict.fromkeys(metrics, 0)
-
-        # TODO: do lr decay here
-        # the train_model fn can take a list of callbacks (like keras)
-        # e.g., [lr_decay, early_stop]
+        train_metrics_init = dict.fromkeys(metrics, 0)
 
         # Start training iterations (for this epoch)
         # print(" Training Phase (In Progress) ".center(79, "-"))
@@ -252,36 +289,53 @@ def train_model(
             loss_fn,
             scaler,
             from_logits,
-            metrics,
+            train_metrics_init,
             metrics_fn,
-            device,
+            epoch=epoch,
+            device=device,
         )
 
         # plot the validation metrics + save to history (dict)
         # print(f" epoch {epoch}'s metric(s) (training) ".center(79, "."))
         print()
-        for key, value in train_metrics.items():
-            print(f"{key+':':<15} {value:>5.2f}")
+        # used reversed() to make 'loss' the first item
+        for key, value in reversed(train_metrics.items()):
+            print(f"{key+':':<20} {value:>5.2f}")
             history[key].append(value)
         # print(" Training Phase (Done) ".center(79, "-"))
 
         if val_loader:
-            # TODO: use metrics dict (but with 'val_' prefix) to automate things
             # print(" Validation Phase (In Progress) ".center(79, "-"))
 
-            val_accu, val_dice = validation_fn(
-                val_loader, model, from_logits, metrics_fn=metrics_fn, device=device
+            # add 'val_' to each metric
+            # create/reset metrics values to 0 (for each epoch)
+            val_metrics_init = dict.fromkeys([f"val_{metric}" for metric in metrics], 0)
+
+            val_metrics = validation_fn(
+                val_loader,
+                model,
+                loss_fn,
+                from_logits,
+                val_metrics_init,
+                metrics_fn,
+                device=device,
             )
 
             # plot the validation metrics
             # print(f" epoch {epoch}'s metric(s) (validation) ".center(79, "."))
-            print(f'{"val_accuracy:":<15} {val_accu.item():>5.2f}')
-            print(f'{"val_dice:":<15} {val_dice.item():>5.2f}')
-            history["val_accuracy"].append(val_accu.item())
-            history["val_dice"].append(val_dice.item())
+            for key, value in reversed(val_metrics.items()):
+                print(f"{key+':':<20} {value:>5.2f}")
+                history[key].append(value)
+            # print(f'{"val_accuracy:":<15} {val_accu.item():>5.2f}')
+            # print(f'{"val_dice:":<15} {val_dice.item():>5.2f}')
+            # history["val_accuracy"].append(val_accu.item())
+            # history["val_dice"].append(val_dice.item())
             # print(" Validation Phase (Done) ".center(79, "-"))
 
             # print some examples to a folder
+
+        # decay lr_rate (must be at the end of each epoch)
+        scheduler.step()
 
     # save the trained model
     if save_model:
@@ -293,8 +347,6 @@ def train_model(
         save_checkpoint(
             checkpoint, dirname=save_checkpoint_path, filename=save_checkpoint_name
         )
-
-    #
 
     return history
 
