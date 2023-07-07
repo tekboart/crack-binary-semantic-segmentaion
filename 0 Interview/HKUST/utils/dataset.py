f9d6fc07765b773709b7c1876d5b454625d55416
tekboart <tekboart@gmail.com>
2023-07-08 00:04:35 +0330

    (1) minor refactoring

--- 0 Interview/HKUST/utils/dataset.py
+++ 0 Interview/HKUST/utils/dataset.py
@@ -1,22 +1,8 @@
 import os
-import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader, Sampler, Subset
-import torchvision
-
-import albumentations as A
-
-# import albumentations.augmentations.functional as F
-from albumentations.pytorch import ToTensorV2
-
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from PIL import Image
-
 from typing import Union
-
-
-# TODO: where is the shuffle part? maybe in DataLoader
-
 
 class SegmentaionDataset(Dataset):
     """
@@ -28,6 +14,8 @@ class SegmentaionDataset(Dataset):
         Set the start and end indices, to avoid
         >>> SegmentationDataset(image_dir, mask_dir, subset=[0, 10])
         takes only 10 samples to create the dataset (i.e., [0:10])
+    data_format: st
+        define whether the final (img, mask) be channels_first (default) or channels_last
     """
 
     def __init__(
@@ -39,6 +27,7 @@ class SegmentaionDataset(Dataset):
         mask_suffix: str = "_mask",
         transform=None,
         num_classes: int = 1,
+        data_format: str = 'channels_first',
         subset: Union[tuple, list] = None,
     ):
         super().__init__()
@@ -54,6 +43,7 @@ class SegmentaionDataset(Dataset):
         self.mask_suffix = mask_suffix
         self.num_classes = num_classes
         self.transform = transform
+        self.data_format = data_format
 
     def __len__(self) -> int:
         """
@@ -73,97 +63,50 @@ class SegmentaionDataset(Dataset):
             ),
         )
 
-        # NumPy implementation (channels_last format)
-        # we used np.array, as opposed to PIL, as it will be converted to other formats (e.g., torch.Tensor) much easier
-        # TODO: as numpy cannot run on GPU maybe it's better to use pure torch tensors instead.
-        # TODO: why we define each element in channels_last format???
-        # .convert('RGB') as img is RGB (3 channels)
+        # TODO: why we define each element in channels_last format??? for self.transform
         image = np.array(Image.open(img_path).convert("RGB"))
-        # make channels_first
-        # image = np.moveaxis(image, -1, 0)
-        # print('img shape', image.shape)
-        # .convert('L') as mask is grayscale
         mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
-        # print('mask shape', mask.shape)
-        # (H, W) --> (C=1, H, W) (an img must be 3darray not matrix)
-        # mask = np.expand_dims(mask, 0)
-        # print('mask shape', mask.shape)
         if self.num_classes == 1:
-            # We need 0/1 for each pixel (not 0.0 or 255.), this is needed for sigmoid/softmax fn.
+            # make the mask binary (float) (needed for sigmoid/softmax)
             mask = np.where(mask == 255.0, 1.0, mask)
+            # make the mask a 3DTensor: (H, W) --> (H, W, C=1)
+            mask = np.expand_dims(mask, -1)
         elif self.num_classes > 1:
-            # convert a gray img with pixel values [0,1,..,C] to a (C, H, W) 3Dtensor
-            # in each channel pixels must be either 0 or 1 (0: is_not_class_obj, 1:is_class_obj)
+            # convert a gray img with pixel values [0,1,..,C] to a (H, W, C=num_classes) 3Dtensor
+            # in each channel pixels must be binary float (0.0: is_not_class_obj, 1.0:is_class_obj)
             pass
-
-        # # PyTorch implementation (channels_first format)
-        # image = torchvision.io.read_image(img_path, torchvision.io.ImageReadMode.RGB).float()
-        # # print('img shape', image.shape)
-        # mask = torchvision.io.read_image(mask_path, torchvision.io.ImageReadMode.GRAY).float()
-        # # print('mask shape', mask.shape)
-
-        # if self.num_classes == 1:
-        #     # We need 0/1 for each pixel (not 0.0 or 255.), this is needed for sigmoid/softmax fn.
-        #     mask = torch.where(mask == 255.0, 1.0, mask)
-        # elif self.num_classes > 1:
-        #     # convert a gray img with pixel values [0,1,..,C] to a (C, H, W) 3Dtensor
-        #     # in each channel pixels must be either 0 or 1 (0: is_not_class_obj, 1:is_class_obj)
-        #     pass
 
         if self.transform:
             # This implementation works with transfomrs that input both img and mask (and return both)
             # e.g., alumentations and/or torchvision transformers
             augmentations = self.transform(image=image, mask=mask)
             image = augmentations.get("image")
-            # print('augmentations image:', augmentations['image'].shape)
             mask = augmentations.get("mask")
-            # print('augmentations mask:', augmentations['mask'].shape)
-            # print('type mask', augmentations['mask'].__class__)
 
-        # make the image, mask channels_first
+        # make the (image, mask) channels_first (as needed by pytorch models)
         # Only if not using alumentations's ToTensorV2 (in self.transform)
         # which automatically reshape's the img, mask for pytorch models (caused error and shape mismatch)
-        image = np.moveaxis(image, -1, 0)
-        # print('image shape', image.shape)
-        mask = np.expand_dims(mask, 0)
-        # print('mask shape', mask.shape)
+        if self.data_format == 'channels_first':
+            image = np.moveaxis(image, -1, 0)
+            assert image.shape[0] == 3
+            mask = np.moveaxis(mask, -1, 0)
+            assert mask.shape[0] == self.num_classes
+        # elif self.data_format == 'channels_last':
+        #     assert image.shape[-1] == 3
+        #     mask = np.expand_dims(mask, -1)
 
         return image, mask
 
-
-class DatasetSampler(Sampler):
-    """
-    a custom sampler for the dataset loader avoiding recreating the dataset
-        just creating a new loader for each different sampling.
-
-
-    Examples
-    --------
-    >>> trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-    >>>                                     download=True, transform=transform)
-
-    >>> sampler1 = YourSampler(your_mask)
-    >>> sampler2 = YourSampler(your_other_mask)
-    >>> trainloader_sampler1 = torch.utils.data.DataLoader(trainset, batch_size=4,
-    >>>                                         sampler = sampler1, shuffle=False, num_workers=2)
-    >>> trainloader_sampler2 = torch.utils.data.DataLoader(trainset, batch_size=4,
-    >>>                                         sampler = sampler2, shuffle=False, num_workers=2)
-    """
-
-    def __init__(self, mask):
-        self.mask = mask
-
-    def __iter__(self):
-        return (self.indices[i] for i in torch.nonzero(self.mask))
-
-    def __len__(self):
-        return len(self.mask)
+    # TODO: create the __iter__ methodd (must be like tf.Dataset to use next(iter()))
+    # def __iter__(self):
+    #     pass
 
 
 def get_loaders(
     train_ds: Dataset,
-    val_ds: Dataset,
-    batch_size,
+    val_ds: Dataset = None,
+    test_ds: Dataset = None,
+    batch_size: int = 16,
     num_workers: int = 4,
     pin_memory: bool = True,
 ):
@@ -180,15 +123,30 @@ def get_loaders(
         pin_memory=pin_memory,
         shuffle=True,
     )
-    val_dataloader = DataLoader(
-        val_ds,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        pin_memory=pin_memory,
-        shuffle=False,
-    )
 
-    return train_dataloader, val_dataloader
+    if val_ds:
+        val_dataloader = DataLoader(
+            val_ds,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            shuffle=False,
+        )
+    else:
+        val_dataloader = None
+
+    if test_ds:
+        test_dataloader = DataLoader(
+            test_ds,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            shuffle=False,
+        )
+    else:
+        test_dataloader = None
+
+    return train_dataloader, val_dataloader, test_dataloader
 
 
 ###############################################################################
