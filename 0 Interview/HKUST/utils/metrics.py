315cc3309fc9ccf91bd790bf12d1907798f51ade
tekboart <tekboart@gmail.com>
2023-07-08 09:01:39 +0330

    (1) turned dice, jaccard, & accu fns to class, (2) written a test for this module, (3) fixed the dice/jaccard (had removed the (preds > thresh).float() part)

--- 0 Interview/HKUST/utils/metrics.py
+++ 0 Interview/HKUST/utils/metrics.py
@@ -1,114 +1,205 @@
 import torch
+from torchmetrics.metric import Metric
 
-
-# TODO: maybe create a class for each custom eval metrics and set a __str__ if we print it
-# we can subclass pytorch lighnting's TensorMetric to implement metrics
-# as this subclass handles automated DDP syncing and converts all inputs and outputs to tensors.
-
-
-def dice_binary_segment(
-    preds: torch.Tensor, targets: torch.Tensor, from_logits: bool, smooth: float = 1.0
-) -> int:
+# make this class a subset of torchmetrics.metric.Metric
+# needs update() and compute() overrriding (its an ABC class)
+import torch.nn as nn
+class DiceBinarySegment(nn.Module):
     """
     Calculate the dice metric (aka Sørensen–Dice coefficient) for binary segmentation
 
-    Parameters
-    ----------
-    preds: torch.Tensor
-    targets: torch.Tensor
+    Atributes
+    ---------
     from_logits: bool
         Are predictions logits (output without activation_fn)?
     smooth: float, optional
-
-    Returns
-    -------
-    int
-        The binary segmentation dice for a batch of images.
     """
-    if from_logits:
-        preds = torch.sigmoid(preds)
 
-    # flatten label and prediction tensors
-    preds = preds.view(-1).float()
-    targets = targets.view(-1).float()
+    def __init__(
+        self, from_logits: bool = True, smooth: float = 1e-5, thresh: float = 0.5
+    ):
+        super().__init__()
+        self.from_logits = from_logits
+        self.smooth = smooth
+        self.thresh = thresh
 
-    # calc |A ∩ B|
-    intersection = (preds * targets).sum().float()
-    # calc |A| + |B|
-    summation = (preds + targets).sum()
-    # calc 2 * |A ∩ B| / |A| + |B|
-    dice = (2.0 * intersection + smooth) / (summation + smooth)
+    def __call__(self, preds: torch.Tensor, targets: torch.Tensor):
+        """
+        Parameters
+        ----------
+        preds: torch.Tensor
+        targets: torch.Tensor
+        from_logits: bool
+            Are predictions logits (output without activation_fn)?
+        smooth: float, optional
 
-    return dice
+        Returns
+        -------
+        int
+            The binary segmentation dice for a batch of images.
+        """
+        if self.from_logits:
+            preds = torch.sigmoid(preds)
+
+        # make each pixel values binary (0 or 1)
+        preds = (preds >= self.thresh).float()
+
+        assert preds.shape == targets.shape
+        # flatten label and prediction tensors
+        preds = preds.view(-1).float()
+        targets = targets.view(-1).float()
+
+        # calc |A ∩ B|
+        intersection = (preds * targets).sum().float()
+        # calc |A| + |B|
+        # summation = (preds + targets).sum()
+        summation = (preds.square() + targets.square()).sum().float()
+        # calc 2 * |A ∩ B| / |A| + |B|
+        dice = (2.0 * intersection + self.smooth) / (summation + self.smooth)
+
+        return dice
 
 
-def jaccard_binary_segment(
-    preds: torch.Tensor, targets: torch.Tensor, from_logits: bool, smooth: float = 1.0
-) -> int:
+class JaccardBinarySegment:
     """
     Calculate the Jaccard metric (aka IOU) for binary segmentation
 
-    Parameters
-    ----------
-    preds: torch.Tensor
-    targets: torch.Tensor
+    Atributes
+    ---------
     from_logits: bool
         Are predictions logits (output without activation_fn)?
     smooth: float, optional
-
-    Returns
-    -------
-    int
-        The binary segmentation jaccard for a batch of images.
     """
-    if from_logits:
-        preds = torch.sigmoid(preds)
 
-    # flatten label and prediction tensors
-    preds = preds.view(-1).float()
-    targets = targets.view(-1).float()
+    def __init__(
+        self, from_logits: bool = True, smooth: float = 1e-5, thresh: float = 0.5
+    ):
+        super().__init__()
+        self.from_logits = from_logits
+        self.smooth = smooth
+        self.thresh = thresh
 
-    # calc |A ∩ B|
-    intersection = (preds * targets).sum().float()
-    # calc |A| + |B|
-    total = (preds + targets).sum()
-    # calc |A ∪ B| = |A| + |B| - |A ∩ B|
-    union = total - intersection
+    def __call__(self, preds: torch.Tensor, targets: torch.Tensor):
+        """
+        Parameters
+        ----------
+        preds: torch.Tensor
+        targets: torch.Tensor
+        from_logits: bool
+            Are predictions logits (output without activation_fn)?
+        smooth: float, optional
 
-    jaccard = (intersection + smooth) / (union + smooth)
+        Returns
+        -------
+        int
+            The binary segmentation jaccard for a batch of images.
+        """
+        if self.from_logits:
+            preds = torch.sigmoid(preds)
 
-    return jaccard
+        # flatten label and prediction tensors
+        preds = preds.view(-1).float()
+        targets = targets.view(-1).float()
+
+        # make each pixel values binary (0 or 1)
+        preds = (preds >= self.thresh).float()
+
+        # calc |A ∩ B|
+        intersection = (preds * targets).sum().float()
+        # calc |A| + |B|
+        total = (preds + targets).sum()
+        # calc |A ∪ B| = |A| + |B| - |A ∩ B|
+        union = total - intersection
+
+        jaccard = (intersection + self.smooth) / (union + self.smooth)
+
+        return jaccard
 
 
-def accuracy_binary_segment(
-    preds, targets, from_logits: bool, thresh: float = 0.5
-) -> int:
+class AccuracyBinarySegment:
     """
-    Calculate the accuracy for binary segmentation
+    Calculate the Accuracy for binary segmentation
 
-    Parameters
-    ----------
-    preds: torch.Tensor
-    targets: torch.Tensor
+    Atributes
+    ---------
     from_logits: bool
         Are predictions logits (output without activation_fn)?
-    thresh: float, optional
-
-    Returns
-    -------
-    int
-        The binary segmentation accuracy for a batch of images.
+    smooth: float, optional
     """
-    if from_logits:
-        preds = torch.sigmoid(preds)
 
-    # make each pixel values binary (0 or 1)
-    preds = (preds > thresh).float()
-    # calc #pixels in this img_batch that were classified correctly
-    num_correct_pixels = (preds == targets).sum()
-    # calc the total #pixels in this img_batch
-    num_pixels = torch.numel(preds)
+    def __init__(
+        self, from_logits: bool = True, smooth: float = 1e-5, thresh: float = 0.5
+    ):
+        super().__init__()
+        self.from_logits = from_logits
+        self.smooth = smooth
+        self.thresh = thresh
 
-    accu = num_correct_pixels / num_pixels
+    def __call__(self, preds: torch.Tensor, targets: torch.Tensor):
+        """
+        Parameters
+        ----------
+        preds: torch.Tensor
+        targets: torch.Tensor
+        from_logits: bool
+            Are predictions logits (output without activation_fn)?
+        smooth: float, optional
 
-    return accu
+        Returns
+        -------
+        int
+            The binary segmentation jaccard for a batch of images.
+        """
+        if self.from_logits:
+            preds = torch.sigmoid(preds)
+
+        # make each pixel values binary (0 or 1)
+        preds = (preds >= self.thresh).float()
+
+        # calc #pixels in this img_batch that were classified correctly
+        num_correct_pixels = (preds == targets).sum()
+        # calc the total #pixels in this img_batch
+        num_pixels = torch.numel(preds)
+
+        accu = num_correct_pixels / num_pixels
+
+        return accu
+
+
+###############################################################################
+# For testing
+###############################################################################
+if __name__ == "__main__":
+    from torchmetrics.classification import Dice, BinaryJaccardIndex
+
+    # Compare our custom metircs with torchmetrics counterparts
+    dice_torch = Dice(num_classes=1, average="micro")
+    jaccard_torch = BinaryJaccardIndex()
+
+    # generate pred and target masks of shape (N=32, C=5, H=64, W=64)
+    preds = torch.randint(
+        low=0, high=2, size=(32, 5, 64, 64), dtype=torch.float16
+    ).view(-1)
+    targets = torch.randint(
+        low=0, high=2, size=(32, 5, 64, 64), dtype=torch.int16
+    ).view(-1)
+
+    print(f"{'dice (me):':<20} {DiceBinarySegment(from_logits=False)(preds, targets)}")
+    # outputs: 4995531141757965
+    print(f"{'dice (torch):':<20} {dice_torch(preds, targets)}")
+    # outputs: 0.4995531141757965
+    print(f"{'jaccard (me):':<20} {JaccardBinarySegment(from_logits=False)(preds, targets)}")
+    # outputs: 0.33293622732162476
+    print(f"{'jaccard (torch):':<20} {jaccard_torch(preds, targets)}")
+    # outputs: 0.33293622732162476
+
+    # Check wether they output 1.0, when predictions are 100% accurate (complete overlap)
+    assert all(
+        [
+            1.0,
+            DiceBinarySegment(from_logits=False)(preds=targets.float(), targets=targets),
+            dice_torch(targets.float(), targets),
+            JaccardBinarySegment(from_logits=False)(preds=targets.float(), targets=targets),
+            jaccard_torch(targets.float(), targets),
+        ]
+    )
